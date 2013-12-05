e5e51994a16ac9c532b2c56c6de0711a8678b385
phospher <phospher@qq.com>
2013-12-05 11:08:50 +0800

    HadoopStreamAdapter.java

---
+++ src/com/phospher/DistributedTaskScheduler/hadoop/HDFSFileAdapter.java
@@ -0,0 +1,12 @@
+package com.phospher.DistributedTaskScheduler.hadoop;
+
+import java.io.*;
+import org.apache.hadoop.conf.*;
+
+public class HDFSFileAdapter implements HadoopStreamAdapter {
+
+	public InputStream getInputStream(Configuration conf) {
+		return null;
+	}
+
+}
---
+++ src/com/phospher/DistributedTaskScheduler/hadoop/HadoopStreamAdapter.java
@@ -0,0 +1,8 @@
+package com.phospher.DistributedTaskScheduler.hadoop;
+
+import java.io.*;
+import org.apache.hadoop.conf.*;
+
+public interface HadoopStreamAdapter {
+	InputStream getInputStream(Configuration conf);
+}
